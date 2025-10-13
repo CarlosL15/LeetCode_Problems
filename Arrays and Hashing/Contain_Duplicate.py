5fe292491a79649ee4a47074a62f1fafdab87f9f
Carlos Lara <carloslara8426@gmail.com>
2025-10-13 17:49:20 -0400

    Saved files and updated project

---
+++ Arrays and Hashing/Contain_Duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) < len(nums)
+
+        if List.contains(duplicate):
+            return true
+        else:
+            false 
